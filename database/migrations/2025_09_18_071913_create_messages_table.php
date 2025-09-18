b9cfd0ac954a999e4df9eca1c079843459c50c82
John Aerial J. Azcune <65322242+STICKnoLOGIC@users.noreply.github.com>
2025-09-18 16:41:00 +0800

    Add migrations for messages and message_views tables
    Introduces two new migration files: one for creating the 'messages' table with fields for token, message, limit, expiration, and soft deletes; and another for 'message_views' to track views of messages, including viewer IP, user agent, and timestamp, with a foreign key to 'messages'.

---
+++ database/migrations/2025_09_18_071913_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('token', 64)->unique();
+            $table->string('message', 150);
+            $table->unsignedInteger('limit')->default(1);
+            $table->unsignedBigInteger('expires_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
