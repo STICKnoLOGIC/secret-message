796ef1e194a923aa3d15c82f5eeb3a5dbe45e59a
STICKnoLOGIC <65322242+STICKnoLOGIC@users.noreply.github.com>
2025-11-06 22:53:36 +0800

    Create: Error Handler

---
+++ resources/views/error/any.blade.php
@@ -0,0 +1,18 @@
+@extends('messages.ads')
+
+@section('content')
+    <div class="relative flex items-center justify-center md:justify-end h-40 md:h-96 w-full overflow-hidden">
+        <!-- Background text -->
+        <span class="absolute inset-0 flex items-center justify-center text-[50vw] md:text-[20vw] font-black text-amber-600 dark:text-amber-500 select-none leading-none pointer-events-none">
+        404
+    </span>
+
+        <!-- Foreground content -->
+        <div class="z-1 text-center bg-gray-100 dark:bg-gray-900 p-0 w-full md:w-2/4 border-t border-b border-t-amber-600 dark:border-t-amber-500 border-b-amber-600 dark:border-b-amber-500">
+            <h2 class="text-xl md:text-2xl lg:text-3xl font-semibold">Page Not Found</h2>
+        </div>
+    </div>
+    <p class="w-full text-center wrap-break-word"> "{{ $message ?? 'Something went wrong.' }}"</p>
+
+@endsection
+
