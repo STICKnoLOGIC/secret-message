eae2947f778ec7b3940d34e0e7cc19fc09aff138
John Aerial J. Azcune <65322242+STICKnoLOGIC@users.noreply.github.com>
2025-11-20 17:22:03 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -16,32 +15,31 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions){
         $exceptions->render(function (Throwable $e) {
-            if ($e instanceof HttpExceptionInterface) {
-                $status = $e->getStatusCode();
-            } else {
-                $status = $e->getCode();
+            if ($e instanceof Symfony\Component\HttpKernel\Exception\HttpException) {
+                $status = $e->getStatusCode() ?? 500;
+            
+                if ($status === 503) {
+                    return response()->view('error.maintenance', [], 503);
+                }
+
+                $safeMessage = match($status) {
+                    404 => 'The Message you are looking for could not be found.',
+                    403 => 'You are not authorized to access this page.',
+                    419 => 'Your session has expired. Please refresh the page.',
+                    500 => 'Something went wrong. Please try again later.',
+                };
+
+                return response()->view('error.any', [
+                    'code' => $status,
+                    'title' => 'Something went wrong',
+                    'message' => $safeMessage ?? $e->getMessage(),
+                ], $status);
             }
-
-            if ($e instanceof Symfony\Component\HttpKernel\Exception\HttpException
-            && $status === 503) {
-                return response()->view('error.maintenance', [], 503);
-            }
-
-            // if ($status < 100 || $status >= 600) {
-            //     $status = 500;
-            // }
-
-            $safeMessage = match($status) {
-                404 => 'The Message you are looking for could not be found.',
-                403 => 'You are not authorized to access this page.',
-                419 => 'Your session has expired. Please refresh the page.',
-                500 => 'Something went wrong. Please try again later.',
-            };
 
             return response()->view('error.any', [
-                'code' => $status,
+                'code' => 500,
                 'title' => 'Something went wrong',
-                'message' => $safeMessage ?? $e->getMessage(),
-            ], $status);
+                'message' => 'An unexpected error occurred. Please try again later.',
+            ], 500);
         });
     })->create();
